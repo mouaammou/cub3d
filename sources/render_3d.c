6617ab078115810e0be3cf62f064c3c0f0c181a2
Mouad Ouaammou <mouaammo@e3r10p3.1337.ma>
2023-09-20 14:03:03 +0100

    the fucking cube is ready

--- sources/render_3d.c
+++ sources/render_3d.c
@@ -6,11 +6,43 @@
 /*   By: mouaammo <mouaammo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 12:58:50 by mouaammo          #+#    #+#             */
-/*   Updated: 2023/09/20 13:37:54 by mouaammo         ###   ########.fr       */
+/*   Updated: 2023/09/20 14:02:17 by mouaammo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
+
+void	color_sky(t_cub3d *data)
+{
+	//     rect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT/2);
+	int i = 0;
+	while (i < NUM_RAYS)
+	{
+		int a = 0;
+		while (a < WINDOW_HEIGHT/2)
+		{
+			put_color(data, i, a, 0x5DADE2);
+			a++;
+		}
+		i++;
+	}
+}
+
+void	color_floor(t_cub3d *data)
+{
+	//     rect(0, WINDOW_HEIGHT/2, WINDOW_WIDTH, WINDOW_HEIGHT)
+	int i = 0;
+	while (i < NUM_RAYS)
+	{
+		int a = 0;
+		while (a < WINDOW_HEIGHT)
+		{
+			put_color(data, i, a + WINDOW_HEIGHT / 2, 0xBFC9CA);
+			a++;
+		}
+		i++;
+	}
+}
 
 void	render_cube_3d(t_cub3d *data)
 {
@@ -18,18 +50,27 @@ void	render_cube_3d(t_cub3d *data)
 	double	distance;
 	double	wall3d_height;
 	double	wall3d_distance;
+	int		color;
 
 	i = 0;
+	color_sky(data);
+	color_floor(data);
 	while (i < NUM_RAYS)
 	{
 		distance = data->myray[i].distance * cos(data->myray[i].ray_angle - data->myplayer.rotation_angle);
 		wall3d_distance = (WINDOW_WIDTH / 2) / tan(FOV_ANGLE / 2);
 		wall3d_height = (TILE_SIZE / distance) * wall3d_distance;
+
 		// draw_line(i * WALL_STRIP_WIDTH, (WINDOW_HEIGHT / 2.0) - (wall3d_height / 2.0), WALL_STRIP_WIDTH, wall3d_height, data, 0xffffff);
 		int a = 0;
+		if (data->myray[i].was_hit_vertical)
+			color = 0x229954;
+		else
+			color = 0x7D6608;
+		
 		while (a < wall3d_height)
 		{
-			put_color(data, i * WALL_STRIP_WIDTH, a + WINDOW_HEIGHT / 2 - wall3d_height / 2, 0xffffff);
+			put_color(data, i * WALL_STRIP_WIDTH, a + WINDOW_HEIGHT / 2 - wall3d_height / 2, color);
 			a++;
 		}
 		i++;
